b423ee576e0a44e63f5002bf1616bb3d2712ab42
nivedhithaannasankar <135960062+nivedhithaannasankar@users.noreply.github.com>
2024-03-31 22:08:52 +0530

    Create Inventory.java

---
+++ Codes/inventory/Inventory.java
@@ -0,0 +1,68 @@
+package inventory;
+
+import item.ItemShelf;
+import item.Item;
+
+public class Inventory {
+    private ItemShelf[] inventory;
+
+    public Inventory (int n){
+        inventory = new ItemShelf[n];
+        initialEmptyInventory();
+    }
+
+    public ItemShelf[] getInventory(){
+        return this.inventory;
+    }
+
+    public void setInventory(ItemShelf[] inventory){
+        this.inventory = inventory;
+    }
+
+    public void initialEmptyInventory(){
+        int startCode = 101;
+        for(int i=0;i<inventory.length;i++){
+            ItemShelf itemShelf = new ItemShelf();
+            itemShelf.setItemCode(startCode);
+            itemShelf.setsoldOut(true);
+            inventory[i] = itemShelf;
+            startCode++;
+        }
+    }
+
+    public void addItem(Item item, int itemCode){
+      for(ItemShelf shelf: inventory){
+        if(shelf.getItemCode() == itemCode){
+            if(shelf.issoldOut() == true){
+                shelf.setItem(item);
+                shelf.setsoldOut(false);
+            }
+        }
+      }
+    }
+
+    public Item getItem(int itemCode){
+        for(ItemShelf shelf: inventory){
+            if(shelf.getItemCode() == itemCode){
+                if(shelf.issoldOut()){
+                    System.out.println("Item is already soldOut");
+                    return new Item();
+                }
+                else{
+                    return shelf.getItem();
+                }
+            }
+        }
+
+        System.out.println("Invalid item Code");
+        return new Item();
+    }
+
+    public void updateSoldOut(int itemCodeNumber){
+        for(ItemShelf shelf: inventory){
+            if(shelf.getItemCode() == itemCodeNumber){
+                shelf.setsoldOut(false);
+            }   
+        }
+    }
+}
